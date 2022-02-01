6ee9c0e673e5726409e85fb76442c928d6b9b6df
Abhishekkarunakaran <abhishekkarunakaran100@gmail.com>
2022-02-01 09:42:50 +0530

    Completed homepage, no funtionality added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_final_fields, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:dictionary/theme/colors.dart';
-import 'package:dictionary/theme/widgets.dart';
+// import 'package:dictionary/theme/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,53 +13,109 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Dtheme _dtheme = Dtheme();
+  List<String> list = ["Hello","Dingy","Pacifist","Essentialism"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: _dtheme.mainColor,
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Stack(
-            children: [
-              Positioned(
-                  left: 0,
-                  bottom: 0,
-                  child: Image.asset('assets/images/dic.png')),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: <Widget>[
-                      Text(
-                        'Dictionary',
-                        style: TextStyle(
-                          fontFamily: 'Noto-Serif',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 50,
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Positioned(
+              left: 0, bottom: 0, child: Image.asset('assets/images/dic.png')),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            height: MediaQuery.of(context).size.height,
+              child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: <Widget>[
+                    Space   r(
+                      flex: 2,
+                    ),
+                    Text(
+                      'Dictionary',
+                      style: TextStyle(
+                        fontFamily: 'Noto-Serif',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                    TextField(
+                      cursorHeight: 18,
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
                           color: Colors.white,
                         ),
-                      ),
-                      TextField(                      
-                        cursorColor: _dtheme.accentColor,
-                        decoration: InputDecoration(
-                          suffixIcon: Icon(Icons.search),
-                          suffixIconColor: _dtheme.accentColor,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide: BorderSide(color: _dtheme.accentColor), 
-                            ),
-    
+                      cursorColor: _dtheme.accentColor,
+                      decoration: InputDecoration(
+                        // focusColor: _dtheme.accentColor,
+                        suffixIcon: IconButton(
+                          onPressed: (){
+                            // ignore: todo
+                            //TODO: call get fn to api with entered word 
+                          },
+                          icon: Icon(
+                            Icons.search,
+                            color: _dtheme.accentColor,
+                          ),
                         ),
-                        
-                      )
-                    ]),
-              ),
-            ],
-          ),
-        ),
+                        // suffixIconColor: _dtheme.accentColor,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: _dtheme.accentColor),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: _dtheme.accentColor),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Recent',
+                      style: TextStyle(
+                        fontFamily: 'Noto-Serif',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: ListView.builder(
+                        padding: const EdgeInsets.only(bottom:20),
+                        physics: const NeverScrollableScrollPhysics(),
+                          itemCount: list.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              onTap: (){
+                                // ignore: todo
+                                // TODO: call the get fn with the word in the list. 
+                              },
+                              child: Text(
+                                list[index],
+                                style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                              ),
+                            );
+                          }),
+                    ),
+                     Spacer(flex:3)
+                  ]),
+            ),
+        ],
       ),
     );
   }
